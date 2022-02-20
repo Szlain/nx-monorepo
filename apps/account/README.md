28a832ed7d947f79f5d296c7613aef8fe2cfd0e4
Russ Atkins <russ@russatkins.co.uk>
2022-02-20 15:01:37 +0000

    add acc

---
+++ apps/account/README.md
@@ -0,0 +1,15 @@
+# account
+
+This application was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build account` to build the application.
+
+## Starting
+
+Run `nx start account` to start/run the application.
+
+## Running unit tests
+
+Run `nx test account` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ apps/account/src/index.ts
@@ -0,0 +1,21 @@
+// import { account } from './app/account';
+
+// console.log(`Running ${account()}`);
+
+import fastify from 'fastify'
+// import { button } from '@tvg/fuse'
+const button = 'woop'
+
+const server = fastify()
+
+server.get('/ping', async (request, reply) => {
+  return `pong\n ${button}`
+})
+
+server.listen(8080, (err, address) => {
+  if (err) {
+    console.error(err)
+    // process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
