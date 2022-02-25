f1e5fcf1fc069432d21614009452f99b1db5e661
Russ Atkins <russ@russatkins.co.uk>
2022-02-25 17:12:31 +0000

    add a load of services

---
+++ apps/react-app/src/app/nx-welcome.tsx
@@ -0,0 +1,21 @@
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ This is a starter component and can be deleted.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ Delete this file and get started with your project!
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+import { JsxButton } from "@tvg/fuse"
+export function NxWelcome({ title }: { title: string }) {
+  return (
+      // <div
+      //   dangerouslySetInnerHTML={{
+      //     __html: `${button}`,
+      //   }}
+      // />
+      <JsxButton />
+  );
+}
+
+export default NxWelcome;
