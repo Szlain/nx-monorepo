f730841f726ca8d4639a7ea4d619ac353bb271cd
Russ Atkins <russ@russatkins.co.uk>
2022-02-28 09:26:56 +0000

    add a spring app - seems messy

---
+++ apps/spring-app/src/main/java/com/example/springapp/SpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example")
+public class SpringAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAppApplication.class, args);
+  }
+}
